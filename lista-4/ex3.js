e39cc07308101d5f74d7d769fdf5d07e12958610
Rafael Albertino Silva <rasrras8592@gmail.com>
2025-09-11 01:08:57 +0000

    add ex 3

---
+++ lista-4/ex3.js
@@ -0,0 +1,5 @@
+let input = require('readline-sync');
+let n1 = input.questionInt("Digite um numero: ")
+
+if(n1>10){console.log('Número maior que 10.')}
+else{console.log('Número menor ou igual a 10.')}
