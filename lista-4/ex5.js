ed7cd898f020e5ae6a738daa8242d4201ae944a3
Rafael Albertino Silva <rasrras8592@gmail.com>
2025-09-17 21:09:42 +0000

    add ex5-10

---
+++ lista-4/ex5.js
@@ -0,0 +1,5 @@
+let input = require('readline-sync');
+let n1 = input.questionInt("Digite um numero: ")
+let n2 = input.questionInt("Digite um numero: ")
+
+if
--- lista-4/ex6.js
+++ lista-4/ex6.js
@@ -3,6 +3,8 @@ let n1 = input.questionInt("Digite um numero: ")
 let n2 = input.questionInt("Digite um numero: ")
 let n3 = input.questionInt("Digite um numero: ")
 
-if(n1>n2, n1>n3){console.log(n1)}
-else{if(n2>n1, n2>n3){console.log(n2)}
-else{console.log(n3)}}
+if(n1>n2){n1=n2}
+if(n1>n3){n1=n3}
+if(n2>n3){n2=n3}
+if(n2>n1){n2=n1}
+{console.log(n1, n2, n3)}
