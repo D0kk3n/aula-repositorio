53f0e9843dc34d5030e602f300fe01228d9e0634
Rafael Albertino Silva <rasrras8592@gmail.com>
2025-10-02 00:38:48 +0000

    add ex9-10

---
+++ lista-1/exercicio10.js
@@ -0,0 +1,7 @@
+let input = require('readline-sync');
+
+let n1 = input.questionInt("Digite o numero: ");
+n1=n1/2
+let var1=n1+3
+let var2=n1+5
+console.log(var1, var2)
---
+++ lista-1/exercicio9.js
@@ -0,0 +1,6 @@
+let input = require('readline-sync');
+
+let n1 = input.questionInt("Digite o numero: ");
+let var1=n1-3
+n1=n1+var1
+console.log(n1)
